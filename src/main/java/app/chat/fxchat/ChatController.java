d581ce2aa1b89d4393808316253b3d2c0476e0cf
BorislavN <borko_nik@abv.bg>
2023-10-18 00:05:38 +0300

    Modified the default FXML file, renamed some files

---
+++ src/main/java/app/chat/fxchat/ChatController.java
@@ -0,0 +1,12 @@
+package app.chat.fxchat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+//TODO: Make it do stuff :D
+public class ChatController {
+    @FXML
+    public void onJoinClick(ActionEvent actionEvent) {
+
+    }
+}
