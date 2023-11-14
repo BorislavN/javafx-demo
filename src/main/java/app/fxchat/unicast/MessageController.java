ad0f15b58ae7448dd2d987865d0c9b14a05e0a2b
BorislavN <borko_nik@abv.bg>
2023-11-14 19:06:12 +0200

    Updated message-view.fxml

--- src/main/java/app/fxchat/unicast/MessageController.java
+++ src/main/java/app/fxchat/unicast/MessageController.java
@@ -1,5 +1,12 @@
 package app.fxchat.unicast;
 
+import javafx.event.ActionEvent;
+
 public class MessageController {
+    public void onEnter(ActionEvent event) {
+    }
+
+    public void onSend(ActionEvent event) {
+    }
     //TODO: This controller will provide the "Direct Message" functionality
 }
